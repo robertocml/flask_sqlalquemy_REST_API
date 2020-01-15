81d8ddb8171600e1ff7ede486b684849742992cc
robertocml <roberto.perezb@outlook.com>
2020-01-14 18:55:19 -0600

    Init config

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
+import os
+
+#Init app
+app = Flask(__name__)
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+#Database
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'db.sqlite')
+app.config['SQLAlchemy_TRACK_MODIFICATIONS'] = False
+
+
+
+@app.route('/', methods=['GET'])
+def get():
+    return jsonify({'msg': 'Hello World'})
+
+
+
+# Run Server
+if __name__ == '__main__':
+    app.run(debug=True)
