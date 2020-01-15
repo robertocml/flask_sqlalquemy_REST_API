a7249db257f59fa8ba455e06c0ed8e32b53890ee
robertocml <roberto.perezb@outlook.com>
2020-01-15 10:27:42 -0600

    " request.json not subscriptable" error fixed

--- app.py
+++ app.py
@@ -51,13 +51,23 @@ def add_product():
     name = request.json['name']
     description = request.json['description']
     price = request.json['price']
-    qty = request.json['qty']
+    qty = request.json['qty']   
 
     new_product = Product(name,description, price, qty)
     db.session.add(new_product)
     db.session.commit()
 
     return product_schema.jsonify(new_product)
+
+
+# Get all products
+@app.route('/product', methods=['GET'])
+def get_products():
+    all_products = Product.query.all()
+    result = products_schema.dump(all_products)
+    print("----------------->",result)
+    return jsonify(result)
+
 
 
 # Run Server
