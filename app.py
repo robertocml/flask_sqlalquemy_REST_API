4b454a38b0f86c4515c3b836167361faa75510bb
robertocml <roberto.perezb@outlook.com>
2020-01-14 19:03:24 -0600

    flake8 added on pipfile

--- app.py
+++ app.py
@@ -11,6 +11,25 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'db.sqlite')
 app.config['SQLAlchemy_TRACK_MODIFICATIONS'] = False
 
+#Init db
+db = SQLAlchemy(app)
+#Init marshmallow
+ma = Marshmallow(app)
+
+#Product Class/Model
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100),unique=True)
+    description = db.Column(db.String(200))
+    price = db.Column(db.Float)
+    qty = db.Column(db.Integer)
+
+    def __init__(self, name, description, price, qty):
+        self.name = name
+        self.description = description
+        self.qty = qty
+
+
 
 
 @app.route('/', methods=['GET'])
